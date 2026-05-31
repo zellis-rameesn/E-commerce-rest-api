a272480f7cc9b11c145464b80fec3601d9c84ce4
zellis-rameesn <rameesnoushad@gmail.com>
2026-05-31 12:23:03 +0530

    Added product handlers

--- internal/server/productHandlers.go
+++ internal/server/productHandlers.go
@@ -9,7 +9,7 @@ import (
 	"github.com/zellis-rameesn/go-ecommerce/internal/utils"
 )
 
-func (s *Server) CreateCategory(c *gin.Context) {
+func (s *Server) createCategory(c *gin.Context) {
 	var req dto.CreateCategoryRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		utils.BadRequestResponse(c, "Invalid data!", err)
@@ -24,7 +24,7 @@ func (s *Server) CreateCategory(c *gin.Context) {
 	utils.CreatedResponse(c, "Category created successfully", category)
 }
 
-func (s *Server) UpdateCategory(c *gin.Context) {
+func (s *Server) updateCategory(c *gin.Context) {
 	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
 	if err != nil {
 		utils.BadRequestResponse(c, "Invalid category id!", err)
@@ -44,7 +44,7 @@ func (s *Server) UpdateCategory(c *gin.Context) {
 	utils.SuccessResponse(c, "Category updated successfully", category)
 }
 
-func (s *Server) DeleteCategory(c *gin.Context) {
+func (s *Server) deleteCategory(c *gin.Context) {
 	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
 	if err != nil {
 		utils.BadRequestResponse(c, "Invalid category id!", err)
@@ -58,7 +58,7 @@ func (s *Server) DeleteCategory(c *gin.Context) {
 	utils.SuccessResponse(c, "Category deleted successfully", nil)
 }
 
-func (s *Server) GetCategories(c *gin.Context) {
+func (s *Server) getCategories(c *gin.Context) {
 	productService := services.NewProductService(s.DB)
 	categories, err := productService.GetCategories()
 	if err != nil {
@@ -66,4 +66,81 @@ func (s *Server) GetCategories(c *gin.Context) {
 		return
 	}
 	utils.SuccessResponse(c, "Categories fetched successfully", categories)
+}
+
+func (s *Server) createProduct(c *gin.Context) {
+	var req dto.CreateProductRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		utils.BadRequestResponse(c, "Invalid data!", err)
+		return
+	}
+	productService := services.NewProductService(s.DB)
+	category, err := productService.CreateProduct(&req)
+	if err != nil {
+		utils.InternalServerErrorResponse(c, "Failed to create product", err)
+		return
+	}
+	utils.CreatedResponse(c, "Product created successfully", category)
+}
+
+func (s *Server) updateProduct(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		utils.BadRequestResponse(c, "Invalid product id!", err)
+		return
+	}
+	var req dto.UpdateProductRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		utils.BadRequestResponse(c, "Invalid data!", err)
+		return
+	}
+	productService := services.NewProductService(s.DB)
+	category, err := productService.UpdateProduct(uint(id), &req)
+	if err != nil {
+		utils.InternalServerErrorResponse(c, "Failed to update product", err)
+		return
+	}
+	utils.SuccessResponse(c, "Product updated successfully", category)
+}
+
+func (s *Server) deleteProduct(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		utils.BadRequestResponse(c, "Invalid product id!", err)
+		return
+	}
+	productService := services.NewProductService(s.DB)
+	if err := productService.DeleteProduct(uint(id)); err != nil {
+		utils.InternalServerErrorResponse(c, "Failed to update product", err)
+		return
+	}
+	utils.SuccessResponse(c, "Product deleted successfully", nil)
+}
+
+func (s *Server) getProducts(c *gin.Context) {
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
+	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "10"))
+
+	productService := services.NewProductService(s.DB)
+	products, meta, err := productService.GetProducts(page, limit)
+	if err != nil {
+		utils.InternalServerErrorResponse(c, "Failed to get product", err)
+		return
+	}
+	utils.PaginationResponse(c, "Products fetched successfully", products, *meta)
+}
+
+func (s *Server) GetProduct(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 32)
+	if err != nil {
+		utils.BadRequestResponse(c, "Invalid product id!", err)
+		return
+	}
+	productService := services.NewProductService(s.DB)
+	product, err := productService.GetProduct(uint(id))
+	if err != nil {
+		utils.InternalServerErrorResponse(c, "Failed to get product", err)
+		return
+	}
+	utils.SuccessResponse(c, "Product fetched successfully", product)
 }
